194abc0eba603dfac20dd850f5004c119023c47f
Jakub <jochlik@email.cz>
2017-10-15 15:28:10 +0200

    basic constructors for neurons and their connections

--- BP-Association/src/backPropagation/Connection.java
+++ BP-Association/src/backPropagation/Connection.java
@@ -1,5 +1,12 @@
 package backPropagation;
 
 public class Connection {
+	final Neuron leftNeuron;
+	final Neuron rightNeuron;
+	
+	public Connection(Neuron leftNeuron, Neuron rightNeuron) {
+		this.leftNeuron = leftNeuron;
+		this.rightNeuron = rightNeuron;
+	}
 
 }
--- BP-Association/src/backPropagation/Neuron.java
+++ BP-Association/src/backPropagation/Neuron.java
@@ -1,6 +1,11 @@
 package backPropagation;
 
 public class Neuron {
+	static int numberOfNeurons = 0;
+	final int neuronID;
 	
-
+	public Neuron() {
+		this.neuronID = numberOfNeurons;
+		numberOfNeurons++;
+	}
 }
